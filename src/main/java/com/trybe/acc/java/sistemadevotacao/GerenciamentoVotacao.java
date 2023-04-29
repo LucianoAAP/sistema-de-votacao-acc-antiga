b7c4dab2e4256a495975731f1f1bbe495762464a
LucianoAAP <lucianoalmeidaap@gmail.com>
2023-04-28 21:39:58 -0300

    conclui os requisitos

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,86 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+public class GerenciamentoVotacao {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfComputado = new ArrayList<String>();
+  private int totalVotos;
+
+  public ArrayList<PessoaCandidata> getPessoasCandidatas() {
+    return pessoasCandidatas;
+  }
+  public void setPessoasCandidatas(ArrayList<PessoaCandidata> pessoasCandidatas) {
+    this.pessoasCandidatas = pessoasCandidatas;
+  }
+  public ArrayList<PessoaEleitora> getPessoasEleitoras() {
+    return pessoasEleitoras;
+  }
+  public void setPessoasEleitoras(ArrayList<PessoaEleitora> pessoasEleitoras) {
+    this.pessoasEleitoras = pessoasEleitoras;
+  }
+  public ArrayList<String> getCpfComputado() {
+    return cpfComputado;
+  }
+  public void setCpfComputado(ArrayList<String> cpfComputado) {
+    this.cpfComputado = cpfComputado;
+  }
+  public int getTotalVotos() {
+    return totalVotos;
+  }
+  public void setTotalVotos(int totalVotos) {
+    this.totalVotos = totalVotos;
+  }
+  
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoaCandidata : this.pessoasCandidatas) {      
+      if (pessoaCandidata.getNumero() == numero) {
+        System.out.println("Número pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    this.pessoasCandidatas.add(new PessoaCandidata(nome, numero));
+  }
+  
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoaEleitora : this.pessoasEleitoras) {      
+      if (pessoaEleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    this.pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
+  }
+  
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    if (this.cpfComputado.contains(cpfPessoaEleitora)) {
+      System.out.println("Pessoa eleitora já votou!");
+      return;
+    }
+    for (PessoaCandidata pessoaCandidata : this.pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numeroPessoaCandidata) {
+        pessoaCandidata.receberVoto();
+        break;
+      }
+    }
+    this.cpfComputado.add(cpfPessoaEleitora);
+    this.totalVotos += 1;
+  }
+  
+  public void mostrarResultado() {
+    if (this.totalVotos == 0) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+      return;
+    }
+    for (int index = 0; index < this.pessoasCandidatas.size(); index += 1) {
+      PessoaCandidata pessoaCandidata = this.pessoasCandidatas.get(index);
+      System.out.println(String.format("Nome: %s - %s votos (%s)", pessoaCandidata.getNome(), pessoaCandidata.getVotos(), calcularPorcentagemVotos(index)));
+    }
+    System.out.println(String.format("Total de votos: %s", this.totalVotos));
+  }
+  
+  private double calcularPorcentagemVotos(int index) {
+    return Math.round(pessoasCandidatas.get(index).getVotos() / this.totalVotos * 100);
+  }
 }
--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,10 +1,121 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+    Scanner scanner = new Scanner(System.in);
+    GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
+    System.out.println("----------- Bem-vindo ao Sistema de Votação -----------");
+    System.out.println("");
+    showRegisterCandidateMenu(scanner, gerenciamentoVotacao);
   }
 
+  private static void showRegisterCandidateMessage() {
+    System.out.println("Cadastrar pessoa candidata?");
+    System.out.println("1 - Sim");
+    System.out.println("2 - Não");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+  }
+  
+  private static void showRegisterVoterMessage() {
+    System.out.println("Cadastrar pessoa eleitora?");
+    System.out.println("1 - Sim");
+    System.out.println("2 - Não");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+  }
+  
+  private static void showVotingMessage() {
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    System.out.println("1 - Votar");
+    System.out.println("2 - Resultado Parcial");
+    System.out.println("3 - Finalizar Votação");
+  }
+  
+  private static void showRegisterCandidateMenu(Scanner scanner, GerenciamentoVotacao gerenciamentoVotacao) {
+    try {
+      showRegisterCandidateMessage();
+      int option = scanner.nextInt();
+      if (option == 1) {
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String name = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int number = scanner.nextInt();
+        gerenciamentoVotacao.cadastrarPessoaCandidata(name, number);
+        showRegisterCandidateMenu(scanner, gerenciamentoVotacao);
+      } else if (option == 2) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("----------- Cadastre as pessoas eleitoras -----------");
+        System.out.println("");
+        showRegisterVoterMenu(scanner, gerenciamentoVotacao);
+      } else {
+        System.out.println("Entre com uma opção válida!");
+        showRegisterCandidateMenu(scanner, gerenciamentoVotacao);
+      }
+    } catch (InputMismatchException e) {
+      System.out.println("Entre com uma opção válida!");
+      showRegisterCandidateMenu(scanner, gerenciamentoVotacao);
+    }
+  }
+  
+  private static void showRegisterVoterMenu(Scanner scanner, GerenciamentoVotacao gerenciamentoVotacao) {
+    try {
+      showRegisterVoterMessage();
+      int option = scanner.nextInt();
+      if (option == 1) {
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String name = scanner.next();
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpf = scanner.next();
+        gerenciamentoVotacao.cadastrarPessoaEleitora(name, cpf);;
+        showRegisterVoterMenu(scanner, gerenciamentoVotacao);
+      } else if (option == 2) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("----------- Votação iniciada! -----------");
+        System.out.println("");
+        showVotingMenu(scanner, gerenciamentoVotacao);
+      } else {
+        System.out.println("Entre com uma opção válida!");
+        showRegisterVoterMenu(scanner, gerenciamentoVotacao);
+      }
+    } catch (InputMismatchException e) {
+      System.out.println("Entre com uma opção válida!");
+      showRegisterVoterMenu(scanner, gerenciamentoVotacao);
+    }
+  }
+  
+  private static void showVotingMenu(Scanner scanner, GerenciamentoVotacao gerenciamentoVotacao) {
+    try {
+      showVotingMessage();
+      int option = scanner.nextInt();
+      if (option == 1) {
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpf = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        int number = scanner.nextInt();
+        gerenciamentoVotacao.votar(cpf, number);
+        showVotingMenu(scanner, gerenciamentoVotacao);
+      } else if (option == 2) {
+        gerenciamentoVotacao.mostrarResultado();
+        showVotingMenu(scanner, gerenciamentoVotacao);
+      } else if (option == 3) {
+        gerenciamentoVotacao.mostrarResultado();
+        if (gerenciamentoVotacao.getTotalVotos() == 0) {
+          showVotingMenu(scanner, gerenciamentoVotacao);
+        }
+        scanner.close();
+      } else {
+        System.out.println("Entre com uma opção válida!");
+        showVotingMenu(scanner, gerenciamentoVotacao);
+      }
+    } catch (InputMismatchException e) {
+      System.out.println("Entre com uma opção válida!");
+      showVotingMenu(scanner, gerenciamentoVotacao);
+    }
+  }
 }
